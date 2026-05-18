8619735ab30136c82669b2468960e93f7abd333f
Arsenii Zakharov <arseniizakharov1@gmail.com>
2026-05-18 22:12:40 +0200

    Add awsp beta formula

---
+++ Formula/awsp-beta.rb
@@ -0,0 +1,22 @@
+class AwspBeta < Formula
+  desc "Switch AWS SSO profiles across shell sessions"
+  homepage "https://github.com/nomadsre/awsp"
+  url "https://github.com/nomadsre/awsp/archive/refs/tags/v0.1.0-beta.1.tar.gz"
+  version "0.1.0-beta.1"
+  sha256 "c8d17516606759f642fdabc9b4b922e6c71b58ada0f91e702c42099129c89a63"
+  license "MIT OR Apache-2.0"
+  head "https://github.com/nomadsre/awsp.git", branch: "main"
+
+  depends_on "rust" => :build
+  depends_on "awscli"
+  depends_on "fzf"
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: ".")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/awsp --version")
+    assert_match "awsp shell integration", shell_output("#{bin}/awsp init zsh")
+  end
+end
---
+++ README.md
@@ -0,0 +1,15 @@
+# Homebrew tap for awsp
+
+Install the beta:
+
+```sh
+brew install nomadsre/awsp/awsp-beta
+```
+
+Install the latest `main` build:
+
+```sh
+brew install --HEAD nomadsre/awsp/awsp-beta
+```
+
+Project repo: https://github.com/nomadsre/awsp
