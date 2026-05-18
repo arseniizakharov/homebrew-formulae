758a0149bca64edf5b3b82e81d7775538fe8c46c
Arsenii Zakharov <arseniizakharov1@gmail.com>
2026-05-18 22:15:17 +0200

    Fix awsp beta formula audit

--- Formula/awsp-beta.rb
+++ Formula/awsp-beta.rb
@@ -2,9 +2,8 @@ class AwspBeta < Formula
   desc "Switch AWS SSO profiles across shell sessions"
   homepage "https://github.com/nomadsre/awsp"
   url "https://github.com/nomadsre/awsp/archive/refs/tags/v0.1.0-beta.1.tar.gz"
-  version "0.1.0-beta.1"
   sha256 "c8d17516606759f642fdabc9b4b922e6c71b58ada0f91e702c42099129c89a63"
-  license "MIT OR Apache-2.0"
+  license any_of: ["MIT", "Apache-2.0"]
   head "https://github.com/nomadsre/awsp.git", branch: "main"
 
   depends_on "rust" => :build
